f08865b9394ffd9643cc82dcc8f8efea90d9791c
haritonch <haritonch@yahoo.com>
2021-03-04 21:07:38 +0200

    March 4

--- 4_intersection_of_two_linked_lists.py
+++ 4_intersection_of_two_linked_lists.py
@@ -8,27 +8,16 @@ https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/
 #         self.next = None
 
 class Solution:
-    def length(self, head):
-            ans = 0
-            p = head
-            while p:
-                ans += 1
-                p = p.next
-            return ans
-    
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        nA = length(headA)
-        nB = length(headB)
         pA = headA
         pB = headB
-        if nA > nB:
-            for _ in range(nA - nB):
-                nA -= 1
-                pA = pA.next
-        elif nA < nB:
-            for _ in range(nB - nA):
-                pB = pB.next
         while pA != pB:
-            pA = pA.next
-            pB = pB.next
+            if pA:
+                pA = pA.next
+            else:
+                pA = headB
+            if pB:
+                pB = pB.next
+            else:
+                pB = headA
         return pA
